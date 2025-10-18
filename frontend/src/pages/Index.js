d30642848d38807de313f7d39e5eef5af64af148
ruizmoisesl <moisesruiz2006m@gmail.com>
2025-10-18 15:30:24 -0500

    Cambios de error en produccion

--- frontend/src/pages/Index.js
+++ frontend/src/pages/Index.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 const Index = () => {
